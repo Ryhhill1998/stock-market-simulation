5caaf95aaad2e4e7fc182948789b10586739167d
krutzika <kruthika.ramesh99@gmail.com>
2026-05-09 16:08:59 +0100

    feat: Process limit order - BUY order

--- matching_engine/src/order_book.py
+++ matching_engine/src/order_book.py
@@ -7,8 +7,41 @@ class OrderBook:
         The core engine entry point.
         Matches against existing liquidity or rests the remainder.
         """
-
-        ...
+        if order.quantity <=0 or order.price<=0:
+            return models.MatchResult(
+                order_status=models.OrderStatus.REJECTED,
+                trades=[],
+                remaining_quantity=order.quantity
+            )
+        # Order = BUY 40 @ 101
+        # ask = {100:20, 101:30}
+        # 100 = 20 shares 
+        # 101 = 30 shares
+        
+        trades = []
+        remainder = order.quantity
+        
+        # BUY
+        if order.side == models.Side.BUY:
+            while remainder > 0:
+                best_ask = min(self.ask) # 100
+                available = self.ask[best_ask] #20
+                quant = min(remainder,available) #20
+                trades.append(price=best_ask, quantity=quant, maker_id= "no_idea", taker_id=order.order_id)
+                remainder -= quant # remainder = 40 -20
+                self.ask[best_ask] -= quant # ask {101:30}
+            
+            return models.MatchResult(
+                order_status=models.OrderStatus.FILLED,
+                trades=trades,
+                remaining_quantity=remainder
+            )
+                
+                
+            
+        
+        
+        
 
     def process_market_order(self, side: models.Side, quantity: int) -> models.MatchResult:
         """
